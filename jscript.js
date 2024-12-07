774023f419b16231a53cd292a7e4c66a7f05ae32
learti310 <leart.beri05@gmail.com>
2024-12-07 14:24:54 +0100

    update

--- jscript.js
+++ jscript.js
@@ -3,78 +3,102 @@ let lastNameMsg = document.getElementById("lastNameMsg");
 let emailMsg = document.getElementById("emailMsg");
 let passwordMsg = document.getElementById("passwordMsg");
 let dobMsg = document.getElementById("dobMsg");
-let registerButton = document.getElementById("registerBtn");
+let registerBtn = document.getElementById("registerBtn");
 
-let firstnameRegex = /^[A-Z][a-z]{15}$/;
-let lastnameRegex = /^[A-Z][a-z]{15}$/;
-let emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-let passwordRegex = /^[A-Z][a-zA-Z0-9!@#$%^&*(),.?":{}|<>_+\-=]{7,19}$/;
-let dobRegex = /^(19|20)\d{2}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$/;
-
-const validateFirstName = (firstname) => {
+function validateFirstName(firstname){
+    const firstnameRegex = /^[A-Z][a-z]{1,15}$/;
     return firstnameRegex.test(firstname);
 }
-const validateLastName = (lastname) => {
+
+function validateLastName(lastname){
+    const lastnameRegex = /^[A-Z][a-z]{1,15}$/;
     return lastnameRegex.test(lastname);
 }
-const validateEmail = (email) => {
+
+function validateEmail(email){
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
     return emailRegex.test(email);
 }
-const validatePassword = (password) => {
+
+function validatePassword(password) {
+    let passwordRegex = /^[A-Z][a-zA-Z0-9!@#$%^&*(),.?":{}|<>_+\-=]{7,19}$/;
     return passwordRegex.test(password);
 }
-const validateDob = (dateofbirth) => {
-    return dobRegex.test(dateofbirth);
+
+function validateDob(dob){
+    const dobRegex = /^(19|20)\d{2}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$/;
+    return dobRegex.test(dob);
 }
 
-registerButton.addEventListener("click", (event) => {
+registerBtn.addEventListener("click", function(event) {
     event.preventDefault();
 
-    let firstname = document.getElementById("firstname").value;
-    let lastname = document.getElementById("lastname").value;
-    let email = document.getElementById("email").value;
-    let password = document.getElementById("password").value;
-    let dateofbirth = document.getElementById("dob").value;
+    let firstname = document.getElementById("firstname").value.trim();
+    let lastname = document.getElementById("lastname").value.trim();
+    let email = document.getElementById("email").value.trim();
+    let password = document.getElementById("password").value.trim();
+    let dob = document.getElementById("dob").value.trim();
 
-    if(firstname == ""){
+    console.log(firstname);
+    console.log(lastname);
+
+    firstNameMsg.textContent = "";
+    lastNameMsg.textContent = "";
+    emailMsg.textContent = "";
+    passwordMsg.textContent = "";
+    dobMsg.textContent = "";
+
+    if(!firstname){
         firstNameMsg.style.color="red";
         firstNameMsg.style.fontStyle="italic";
-        firstNameMsg.textContent="First Name is required!";
-    } else if(validateFirstName(firstname)){
-        firstNameMsg.style.color="green";
+        firstNameMsg.textContent="First name is required!";
+    } else if (!validateFirstName(firstname)){
+        firstNameMsg.style.color="red";
         firstNameMsg.style.fontStyle="italic";
-        firstNameMsg.textContent="Wrong!";
-    }  else if (!validateFirstName(firstname)){
-        firstNameMsg.style.color="blue";
-        firstNameMsg.style.fontStyle="italic";
-        firstNameMsg.textContent="Right!";
-    }
-
-    if(lastname == ""){
-        lastNameMsg.style.color="red";
-        lastNameMsg.style.fontStyle="italic";
-        lastNameMsg.textContent="Last Name is required!"
-    }
-
-    if(email == ""){
-        emailMsg.style.color="red";
-        emailMsg.style.fontStyle="italic";
-        emailMsg.textContent="Email is required!"
-    }
-
-    if(password == ""){
-        passwordMsg.style.color="red";
-        passwordMsg.style.fontStyle="italic";
-        passwordMsg.textContent="Password is required!"
-    }
-
-    if(dateofbirth == ""){
-        dobMsg.style.color="red";
-        dobMsg.style.fontStyle="italic";
-        dobMsg.textContent="Date of Birth is required!"
+        firstNameMsg.textContent="First name must start with capital letter!";
     } 
 
-    if(validateFirstName(firstname) && validateLastName(lastname) && validateEmail(email) && validateDob(dateofbirth) && validatePassword(password)){
+    if(!lastname){
+        lastNameMsg.style.color="red";
+        lastNameMsg.style.fontStyle="italic";
+        lastNameMsg.textContent="Last name is required!";
+    } else if (!validateLastName(lastname)){
+        lastNameMsg.style.color="red";
+        lastNameMsg.style.fontStyle="italic";
+        lastNameMsg.textContent="Last name must start with capital letter!";
+    }
+
+    if(!email){
+        emailMsg.style.color="red";
+        emailMsg.style.fontStyle="italic";
+        emailMsg.textContent="Email is required!";
+    } else if (!validateEmail(email)){
+        emailMsg.style.color="red";
+        emailMsg.style.fontStyle="italic";
+        emailMsg.textContent="Email is in the wrong format!";
+    }
+
+    if(!password){
+        passwordMsg.style.color="red";
+        passwordMsg.style.fontStyle="italic";
+        passwordMsg.textContent="Password is required!";
+    } else if (!validatePassword(password)){
+        passwordMsg.style.color="red";
+        passwordMsg.style.fontStyle="italic";
+        passwordMsg.textContent="Password must start with capital letter!";
+    }
+
+    if(!dob){
+        dobMsg.style.color="red";
+        dobMsg.style.fontStyle="italic";
+        dobMsg.textContent="Date of Birth is required!";
+    } else if (!validateDob(dob)){
+        dobMsg.style.color="red";
+        dobMsg.style.fontStyle="italic";
+        dobMsg.textContent="Date of birth is in the wrong format!";
+    }
+
+    if(validateFirstName(firstname) && validateLastName(lastname) && validateEmail(email) && validatePassword(password) && validateDob(dob)){
         firstNameMsg.textContent="";
         lastNameMsg.textContent="";
         emailMsg.textContent="";
@@ -83,3 +107,4 @@ registerButton.addEventListener("click", (event) => {
         window.location="index.html";
     }
 });
+
